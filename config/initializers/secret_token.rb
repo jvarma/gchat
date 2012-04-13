58536aa058ce3a213385ba7348f7db0b7014f683
Jayant Varma <jayant@cellcast.in>
2012-04-13 12:02:14 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gchat::Application.config.secret_token = 'f41c284b5e4bcb9d8990a28014314a365e7e6e04ae07f5cbefcdf8c7f2527f6440c639ad6da7faf55d70a93c22e9303500a8d5818a549e99cd10ceb93e643acd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Gchat::Application.config.session_store :cookie_store, key: '_gchat_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Gchat::Application.config.session_store :active_record_store
