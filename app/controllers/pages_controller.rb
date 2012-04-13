3a33e28de417249fcbe797c78d85f2caf1e33494
Jayant Varma <jayant@cellcast.in>
2012-04-13 12:31:48 +0530

    done with pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
   def home
+  	@title = "home"
   end
 
   def tou
+  	@title = "terms of use"
   end
 
   def help
+  	@title = "help"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def title(page_title)
+		if !page_title
+			"group chat"
+		else
+			"group chat | #{page_title}"
+		end
+	end
 end
